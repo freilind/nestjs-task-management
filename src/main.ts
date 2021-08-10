bd28e9030fc45a95d25f42c453b3e28a4975a0a5
Freilin Manzano <freilin.manzano@externos.bci.cl>
2021-08-10 16:35:56 -0400

    add config prod and postman collection

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ const bootstrap = async () => {
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
-  const port = 3000;
+  const port = process.env.PORT || 3000;
   await app.listen(port);
   logger.log(`Application listening on port ${port}`);
 };
